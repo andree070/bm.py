7c8d538fa987bc9f12727f2930997f80498b1edc
logica_programação <andreluyz0021@gmail.com>
2024-05-20 11:06:43 -0300

    bm.py

---
+++ bm.py
@@ -0,0 +1,10 @@
+nota1 = int(input("digite a nota do primeiro bimestre: "))
+nota2 = int(input("digite a nota do segundo bimestre: "))
+nota3 = int(input("digite a nota do terceiro bimestre:"))
+nota4 = int(input("digite a nota do quarto bimestre:"))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print("a mdeia das notas bimestrais é:", media)
+
+
+
+print(f"A soma entre {numero1} e {numero2} é{soma }")# bm.py
