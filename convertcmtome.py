842e576aeab45390e29798f3cfa9ad1324e740e8
logica_programação <andreluyz0021@gmail.com>
2024-05-21 10:34:37 -0300

    convertcmtome.py

---
+++ convertcmtome.py
@@ -0,0 +1,5 @@
+# Progama para converter metros para centimetros
+
+metros = float(input("Digite o valor em metros: "))
+centimetros = metros * 100
+print(f"{metros} metros equivalem a {centimetros} centimetros. ")
