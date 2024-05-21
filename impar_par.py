e0686903af931314d8a26cc72552508d57a6202f
logica_programação <andreluyz0021@gmail.com>
2024-05-21 07:48:54 -0300

    impar_par.py

---
+++ impar_par.py
@@ -0,0 +1,10 @@
+# Algoritimo para verifica se um número é par ou impar
+
+numero = int(input("Digite um número inteiro; "))
+
+# Verificar se o número é par ou impar
+
+if numero % 2 == 0:
+     print(numero, "é um número par.")
+else:
+     print(numero, "é um número impar.")
