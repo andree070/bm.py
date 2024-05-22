b18f18e45b5caab91e9bb4acc41583c7e15b4823
logica_programação <andreluyz0021@gmail.com>
2024-05-22 09:09:25 -0300

    cllquadrado.py

---
+++ cllquadrado.py
@@ -0,0 +1,13 @@
+# Solicitar ao usuário o valor do lado quadrado
+
+lado = float(input("Digite o valor de lado do quadrado: "))
+
+# Calcular a área do quadrado
+area = lado ** 2
+
+# Calcular o drobo da área
+dobro_area = area * 2
+
+# Mostrar resultado para o usuário
+print(f"A area do quadrado é:{area:.2f}")
+print(f"O dobro da area e: {dobro_area:.2f}")
